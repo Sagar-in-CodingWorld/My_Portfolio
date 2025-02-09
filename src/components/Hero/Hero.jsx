2473661fd2b0e5fa8d5a4dc363aa327f19f4efe1
Sagar Nunia <sagarascoder@gmail.com>
2025-02-10 01:24:15 +0530

    Initial Commit when the website' Navbar And the Hero section have been build

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./Hero.module.css";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Sagar</h1>
+        <p className={styles.description}>
+          I'm a Full-Stack developer with 5 years of experience using React and
+          NodeJs. Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:sagarnunia10@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src="assets/hero/heroImage.png"
+        alt="Iamge of Me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur}></div>
+      <div className={styles.bottomBlur}></div>
+    </section>
+  );
+};
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils.js";
+
+export const Navbar = () => {
+  const [menuOpen, SetMenuOpen] = useState(false);
+  return (
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">
+        Portfolio
+      </a>
+      <div className={styles.menu}>
+        <img
+          src={
+            menuOpen ? "assets/nav/closeIcon.png" : "assets/nav/menuIcon.png"
+          }
+          alt="Menu-Button"
+          className={styles.menuBtn}
+          onClick={() => SetMenuOpen(!menuOpen)}
+        />
+
+        <ul className={`${styles.menuitems} ${menuOpen && styles.menuOpen}`} onClick = {()=> SetMenuOpen(false)}>
+          <li>
+            <a href="#/about">About</a>  
+          </li>
+          <li>
+            <a href="#/experience">Experience</a>
+          </li>
+          <li>
+            <a href="#/projects">Projects</a>
+          </li>
+          <li>
+            <a href="#/contact">CAT</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
